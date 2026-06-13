cd7d48d290c187af50fd7eb77da40eb77c200565
Franklyn Galvin <galvinfranklyn@gmail.com>
2026-06-14 05:46:54 +0800

    ars

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
     <link href="{{ asset('css/home.css') }}" rel="stylesheet">
-    @vite(['resources/js/app.js'])
+    //@vite(['resources/js/app.js'])
     @stack('styles')
 </head>
 <body>
