8b81d71b0fdc3eecf36fcf3cf68185251c78d85a
Franklyn Galvin <galvinfranklyn@gmail.com>
2026-06-14 04:55:11 +0800

    ars

--- public/index.php
+++ public/index.php
@@ -1,14 +1,6 @@
 <?php
-// Tampilkan semua file di direktori saat ini
-echo "Lokasi index.php: " . __DIR__ . "<br>";
-echo "Isi folder saat ini:<br>";
-$files = scandir(__DIR__);
-foreach($files as $file) { echo $file . "<br>"; }
+// Jalur absolut yang pasti benar di Docker Railway
+require '/var/www/html/vendor/autoload.php';
+$app = require_once '/var/www/html/bootstrap/app.php';
 
-// Cek vendor
-echo "<br>Cek vendor di parent folder:<br>";
-echo file_exists(__DIR__ . '/../vendor/autoload.php') ? "Ada di ../vendor" : "TIDAK ADA di ../vendor";
-echo "<br>";
-echo file_exists('/app/vendor/autoload.php') ? "Ada di /app/vendor" : "TIDAK ADA di /app/vendor";
-
-die();
+$app->handleRequest(Illuminate\Http\Request::capture())->send();
