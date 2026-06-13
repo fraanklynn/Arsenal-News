c4ba6ad942d5c389e9a3365d4d57c1a84f73cda4
Franklyn Galvin <galvinfranklyn@gmail.com>
2026-06-14 04:45:13 +0800

    ars

--- public/index.php
+++ public/index.php
@@ -11,10 +11,10 @@ if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
 }
 
 // Register autoloader (sekarang langsung di ./vendor)
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel (sekarang langsung di ./bootstrap)
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
